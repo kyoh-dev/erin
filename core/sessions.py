62c2b43622928a1aa6ed9854b3b96fe017faaca8
kyoh-dev <contact@kyoh.run>
2021-10-16 14:46:36 +1100

    Changing session management to in-memory

---
+++ core/sessions.py
@@ -0,0 +1,21 @@
+from logging import getLogger
+from datetime import datetime, timedelta
+
+from pytz import utc
+
+from core.constants import SESSIONS
+
+logger = getLogger(__name__)
+
+
+def put_session(session_key: str, client_ip: str) -> None:
+    SESSIONS[session_key] = {
+        'client_ip': client_ip,
+        'expires': datetime.now(utc) + timedelta(minutes=15)
+    }
+
+
+def clear_expired_sessions() -> None:
+    for session, values in SESSIONS.items():
+        if values['expires'] <= datetime.now(utc):
+            del SESSIONS[session]
--- core/utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def date_display_fmt(date_string: str) -> str:
-    return datetime.strptime(date_string, "%Y%m%d").strftime("%d/%m/%Y")
-
-
-def date_storage_fmt() -> str:
-    ...
