958db9a3533d20ee43d510a7d5117c45f06d76fd
kyoh-dev <contact@kyoh.run>
2021-10-26 22:25:04 +1100

    Fixing fixture data

--- db/test/2_fixtures.sql
+++ db/test/2_fixtures.sql
@@ -5,11 +5,11 @@ DELETE FROM public.task;
 INSERT INTO
   public.task (assignees, description, due_date)
 VALUES
-  ('Martha Nielsen', 'Clean the kitchen', '2021-10-02'),
-  ('Ulrich Nielsen', 'Do the laundry', '2021-10-02'),
-  ('Katharina Nielsen', 'Cook a communal meal', '2021-10-05'),
+  ('Martha Nielsen, Ulrich Nielsen', 'Clean the kitchen', '2021-10-02'),
+  ('Ulrich Nielsen, Katharina Nielsen', 'Do the laundry', '2021-10-02'),
+  ('Katharina Nielsen, Magnus Nielsen', 'Cook a communal meal', '2021-10-05'),
   ('Magnus Nielsen', 'Clean and tidy the bedrooms', '2021-10-02'),
-  ('Jonas Kahnwald', 'Vacuum and mop the floors', '2021-09-25'),
-  ('Hannah Kahnwald', 'Clean out the fridge', '2021-09-25');
+  ('Jonas Kahnwald, Hannah Kahnwald', 'Vacuum and mop the floors', '2021-09-25'),
+  ('Hannah Kahnwald, Mikhael Kahnwald', 'Clean out the fridge', '2021-09-25');
 
 COMMIT;
